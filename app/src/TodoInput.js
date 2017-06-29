611467de0fa8600b92df729e545be5ad5240de81
wzanbaichi <czsylck@163.com>
2017-06-29 11:46:53 +0800

    this.submit.bind(this)

--- app/src/TodoInput.js
+++ app/src/TodoInput.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 export default class TodoInput extends Component {
     render() {
-        return <input type="text" defaultValue={this.props.content} onKeyPress={this.submit}/>
+        return <input type="text" defaultValue={this.props.content} onKeyPress={this.submit.bind(this)}/>
     }
     submit(e){
         if (e.key === 'Enter'){
