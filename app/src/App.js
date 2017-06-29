b1e410a53eb710d1cdfe48a55a253ce2ed37d0d9
wzanbaichi <czsylck@163.com>
2017-06-29 11:56:15 +0800

    fix value again

--- app/src/App.js
+++ app/src/App.js
@@ -11,10 +11,9 @@ class App extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            newTodo: 'test',
+            newTodo: '',
             todoList: [
-                {id: 1, title: "第一个代办"},
-                {id: 2, title: "第二个代办"}
+
             ]
         }
     }
@@ -22,7 +21,7 @@ class App extends Component {
     render() {
         let todos = this.state.todoList.map((item, index) => {
             return (
-                <li>
+                <li key={index}>
                     <TodoItem todo={item}/>
                 </li>
             )
@@ -31,7 +30,9 @@ class App extends Component {
             <div className="App">
                 <h1>我的代办</h1>
                 <div className='inputWrapper'>
-                    <TodoInput content={this.state.newTodo} onSubmit={this.addTodo}/>
+                    <TodoInput content={this.state.newTodo}
+                               onChange={this.changeTitle.bind(this)}
+                               onSubmit={this.addTodo.bind(this)}/>
                 </div>
                 <ol>
                     {todos}
@@ -39,9 +40,30 @@ class App extends Component {
             </div>
         )
     }
-    addTodo(){
-        console.log('添加一个todo')
+    changeTitle(event){
+        this.setState({
+            newTodo:event.target.value,
+            todoList:this.state.todoList
+        })
+    }
+    addTodo(event){
+        this.state.todoList.push({
+            id:idMaker(),
+            title:event.target.value,
+            statues:null,
+            deleted:false
+        })
+        this.setState({
+            newTodo:'',
+            todoList:this.state.todoList
+        })
     }
 }
 
 export default App;
+let id = 0
+
+function idMaker() {
+    id += 1
+    return id
+}
--- app/src/TodoInput.js
+++ app/src/TodoInput.js
@@ -2,11 +2,16 @@ import React, {Component} from 'react';
 
 export default class TodoInput extends Component {
     render() {
-        return <input type="text" defaultValue={this.props.content} onKeyPress={this.submit.bind(this)}/>
+        return <input type="text" value={this.props.content}
+                      onChange={this.changeTitle.bind(this)}
+                      onKeyPress={this.submit.bind(this)}/>
     }
     submit(e){
         if (e.key === 'Enter'){
-            console.log('回车')
+            this.props.onSubmit(e)
         }
+    }
+    changeTitle(e){
+        this.props.onChange(e)
     }
 }
