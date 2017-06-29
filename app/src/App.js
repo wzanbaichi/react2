a8a06d365f91b5b17120884a273dd2b5e4c672e1
wzanbaichi <czsylck@163.com>
2017-06-29 11:20:45 +0800

    将每个待办封装成 TodoItem

--- app/src/App.js
+++ app/src/App.js
@@ -9,13 +9,19 @@ class App extends Component {
         this.state = {
             newTodo:'test',
             todoList:[
-                {id:1,title:"第一个代办"}
+                {id:1,title:"第一个代办"},
+                {id:2,title:"第二个代办"}
             ]
         }
     }
+
     render(){
         let todos = this.state.todoList.map((item,index)=>{
-            return <li>{item.title}</li>
+            return (
+                <li>
+                    <TodoItem todo={item}/>
+                </li>
+            )
         })
         return(
             <div className="App">
